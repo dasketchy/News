b78507191d087014b927b2c06e2e84b9509c0abe
jmanchuck <jjcheung0000@gmail.com>
2024-06-25 21:44:45 +0100

    Filter away invalid articles, fix home page format
    Co-authored-by: dasketchybbQsauc <dasketchy@users.noreply.github.com>

--- news.py
+++ news.py
@@ -1,66 +1,36 @@
 from flask import Flask, render_template, request
-import random
-import requests
-from datetime import datetime
 from newsapi import NewsApiClient
 
-newsapi = NewsApiClient(api_key='25c14ea863dd4ec7996bba717388eca7')
+from util import filter_for_only_valid_articles, get_article_details
+
+newsapi = NewsApiClient(api_key="25c14ea863dd4ec7996bba717388eca7")
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def home_page():
-    response = newsapi.get_top_headlines(language='en')
-    print(response)
+def home_page() -> None:
+    response = newsapi.get_top_headlines(language="en")
 
-    url_title_list = [
-        {"url": response["articles"][0]["url"], "title": response["articles"][0]["title"]},
-        {"url": response["articles"][1]["url"], "title": response["articles"][1]["title"]},
-        {"url": response["articles"][2]["url"], "title": response["articles"][2]["title"]}
-    ]
-    
-    return render_template("index.html", url_title_list=url_title_list)
+    # Show either first 10 articles, or all articles if total number of articles is less than 10
+    article_list = [get_article_details(response["articles"][i]) for i in range(min(10, len(response["articles"])))]
 
-@app.route("/search_articles", methods=['POST'])
-def search_articles():
+    return render_template("index.html", article_list=article_list)
+
+
+@app.route("/search_articles", methods=["POST"])
+def search_articles() -> None:
     query = request.form["query"]
-    print(query)
 
-    response = newsapi.get_everything(language='en', q=query)
-    print(response)
+    response = newsapi.get_everything(language="en", q=query)
 
-    """
-    {'source': {'id': None, 'name': 'BBC News'}, 
-    'author': 'BBC Sport', 
-    'title': "Real Madrid crowned champions after Barca's defeat at Girona", 
-    'description': 'Real Madrid crowned champions of La Liga for the 36th time after Barcelona lose against Girona.', 
-    'url': 'https://www.bbc.com/sport/football/articles/c0x05j54l8wo', 
-    'urlToImage': 'https://ichef.bbci.co.uk/news/1024/branded_sport/e408/live/d555ab30-0a44-11ef-a26b-0541326d66ca.jpg', 
-    'publishedAt': '2024-05-04T18:42:17Z', 
-    'content': "Real Madrid were crowned champions of La Liga for the 36th time after Barcelona suffered a 4-2 defeat by Girona.\r\nCarlo Ancelotti's side eased to a 3-0 win over Cadiz earlier on Saturday which meant … [+712 chars]"}
-    """
+    articles = filter_for_only_valid_articles(response["articles"])
 
-    # Define the format
-    date_format = "%Y-%m-%dT%H:%M:%SZ"
+    article_list = [get_article_details(article) for article in articles]
 
-    url_title_list = [
-        {
-            "url": article["url"], 
-            "title": article["title"],
-            "author": article["author"],
-            "description": article["description"],
-            "image_url": article["urlToImage"],
-            "date": str(datetime.strptime(article["publishedAt"], date_format).date()),
-            "time": str(datetime.strptime(article["publishedAt"], date_format).time()),
-        } 
-        for article in response["articles"]
-    ]
-
-    return render_template("results.html", url_title_list=url_title_list, query=query)
-
+    return render_template("results.html", article_list=article_list, query=query)
 
 
 @app.route("/login")
-def login_page():
-
+def login_page() -> None:
     return render_template("page.html")
